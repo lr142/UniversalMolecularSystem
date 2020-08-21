6c860eceac6bcfffa7ba0052f2139c4e2fa8fad7
lr142 <lr-mail@163.com>
2020-08-21 22:58:30 +0800

    daily

--- mol22xyz.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import UniversalMolecularSystem as UMS
-import sys
-
-UMS.MainAsMol22XYZ(len(sys.argv),sys.argv)
--- xyz2gaussian.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import UniversalMolecularSystem as UMS
-import sys
-
-UMS.MainAsXYZ2Gaussian(len(sys.argv),sys.argv)
